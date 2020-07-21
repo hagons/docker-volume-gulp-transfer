b4e824bcc61bfed4dc8ca22fd58c76f370d4b205
hagon-desctop <hagons.l@gmail.com>
2020-07-21 19:59:24 +0900

    리팩토링

--- gulpfile.js
+++ gulpfile.js
@@ -2,37 +2,39 @@ const gulp = require('gulp');
 const watch = require('gulp-watch');
 const exec = require('child_process').exec;
 
-gulp.task('default', function () {
-  return watch('./reappay_superadmin', function (f) {
-    if (f.history[0].includes('storage')) return;
-    if (f.history[0].includes('.git')) return;
-    if (f.history[0].includes('node_module')) return;
-    if (f.history[0].includes('vendor')) return;
+const project_name = 'reappay_superadmin';
+const docker_name = `${project_name}_dev_server_src_1`;
+const docker_path = '/var/www/html/';
 
+gulp.task('default', () =>
+  watch(`./${project_name}`, (f) => {
     const src = f.history[0];
-    const dest = src
-      .replace(
-        'C:\\workspace\\2source\\reappay_superadmin_dev_server\\reappay_superadmin\\',
-        ''
-      )
-      .split('\\')
-      .join('/');
+
+    if (src.includes('storage')) return;
+    if (src.includes('.git')) return;
+    if (src.includes('node_module')) return;
+    if (src.includes('vendor')) return;
+
+    const dest = src.replace(f.base, '').split('\\').join('/');
 
     if (f.event === 'unlink') {
-      exec('docker exec reappay_superadmin_dev_server_src_1 rm /var/www/html/' + dest, function (err, stdout, stderr) {
-        if (err) {
-          console.log(err);
-        } else if (stdout && stderr) {
-          console.log(stdout);
-          console.log(stderr);
-        } else {
-          console.log('delete: ' + dest);
+      exec(
+        `docker exec ${docker_name} rm ${docker_path}${dest}`,
+        (err, stdout, stderr) => {
+          if (err) {
+            console.log(err);
+          } else if (stdout && stderr) {
+            console.log(stdout);
+            console.log(stderr);
+          } else {
+            console.log('delete: ' + dest);
+          }
         }
-      })
+      );
     } else {
       exec(
-        'docker cp ' + src + ' reappay_superadmin_dev_server_src_1:/var/www/html/' + dest,
-        function (err, stdout, stderr) {
+        `docker cp ${src} ${docker_name}:${docker_path}${dest}`,
+        (err, stdout, stderr) => {
           if (err) {
             console.log(err);
           } else if (stdout && stderr) {
@@ -44,6 +46,5 @@ gulp.task('default', function () {
         }
       );
     }
-
-  });
-});
+  })
+);
